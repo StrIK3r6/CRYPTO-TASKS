46fb0e0f6485907a6b472057f07ed1ff096382f6
Vishvesh <55224667+StrIK3r6@users.noreply.github.com>
2019-11-14 21:36:44 +0530

    Add files via upload

---
+++ ch17.py
@@ -0,0 +1,27 @@
+from Crypto.Cipher import AES
+
+def aes_ECB_Decrypt(ctext):
+
+    ctext = ctext.decode('base64')
+    
+    key="YELLOW SUBMARIE"
+
+    ptext = AES.new(KEY, AES.MODE_ECB)
+
+    return ptext.decrypt(ctext)
+
+if __name__ == '__main__':
+
+    foo = open("aes_ecb.txt","r")
+    
+    ctext = foo.read()
+    
+    ptext = aes_ECB_Decrypt(ctext)
+
+    foo.close()
+
+    print(ptext)
+    
+
+
+
