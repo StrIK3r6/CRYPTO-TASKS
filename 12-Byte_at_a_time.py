1211f7ddb44533376ef6b445455e541b7f70991b
Vishvesh a.k.a <55224667+StrIK3r6@users.noreply.github.com>
2019-12-25 16:28:07 +0530

    Set-2

--- 12-Byte_at_a_time.py
+++ 12-Byte_at_a_time.py
@@ -50,3 +50,4 @@ if __name__ == '__main__':
 
     key="YELLOW SUBMARINE"
     secret=str(base64.b64decode("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"))
+    exploit(secret)
